31f61cabd5e2fc85da8ace44335b5b8425d21a8a
OBrenson <Oleg-brenyov@yandex.ru>
2021-02-14 11:33:06 +0400

    fix

--- VDCom2Task/src/com/vdcom/Main.java
+++ VDCom2Task/src/com/vdcom/Main.java
@@ -1,15 +1,31 @@
 package com.vdcom;
 
-import java.io.File;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        File file = new File("out.txt");
-        int max = 10000;
+        File file = createAndFillFile();
+        int max = Integer.parseInt(args[0]);
 	    ModifyNumberThread modifyNumberThread1 = new ModifyNumberThread(file, "1 THREAD", max);
         ModifyNumberThread modifyNumberThread2 = new ModifyNumberThread(file, "2 THREAD", max);
         modifyNumberThread1.start();
         modifyNumberThread2.start();
+    }
+
+    private static File createAndFillFile(){
+        File file = new File("out.txt");
+        if(file.exists()) {
+            file.delete();
+        }
+        try {
+            file.createNewFile();
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            bufferedWriter.write(0);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
     }
 }
